5f2b9f34e4796022adb4f052748c7ab78cd8010a
Aaron Bentley <aaron@aaronbentley.com>
2016-10-22 16:49:27 -0400

    Test cleanup_and_exit.

--- tests/test_Plexcomskip.py
+++ tests/test_Plexcomskip.py
@@ -1,6 +1,12 @@
+import contextlib
+import errno
+import os
+import shutil
+import tempfile
 from unittest import TestCase
 
 from PlexComskip import sizeof_fmt
+import PlexComskip
 
 
 class TestSizeofFMT(TestCase):
@@ -18,3 +24,36 @@ class TestSizeofFMT(TestCase):
         self.assertEqual(sizeof_fmt(1024 ** 8 * 1.5), '1.5YB')
         self.assertEqual(sizeof_fmt(1024 ** 9 * 1.5), '1536.0YB')
 
+
+@contextlib.contextmanager
+def temp_dir():
+    temp_dir = tempfile.mkdtemp()
+    try:
+        yield temp_dir
+    finally:
+        try:
+            shutil.rmtree(temp_dir)
+        except OSError as e:
+            if e.errno != errno.ENOENT:
+                raise
+
+
+class TestCleanupAndExit(TestCase):
+
+    def test_no_keep(self):
+        with temp_dir() as work_dir:
+            self.assertIs(os.path.isdir(work_dir), True)
+            os.chdir(work_dir)
+            with self.assertRaises(SystemExit):
+                PlexComskip.cleanup_and_exit(work_dir, keep_temp=False)
+            self.assertNotEqual(os.getcwd(), work_dir)
+            self.assertIs(os.path.exists(work_dir), False)
+
+    def test_keep(self):
+        with temp_dir() as work_dir:
+            self.assertIs(os.path.isdir(work_dir), True)
+            os.chdir(work_dir)
+            with self.assertRaises(SystemExit):
+                PlexComskip.cleanup_and_exit(work_dir, keep_temp=True)
+            self.assertIs(os.path.isdir(work_dir), True)
+            self.assertEqual(os.getcwd(), work_dir)
